f7ba6da976f74badabc2f2db72415d92a88285bb
eiryama7 <pokemonnharimaronn@gmail.com>
2025-11-18 23:14:35 +0900

    Initial Commit

---
+++ filingandquestion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  filingandquestion
+//
+//  Created by 難波映太 on 2025/11/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ filingandquestion/filingandquestionApp.swift
@@ -0,0 +1,17 @@
+//
+//  filingandquestionApp.swift
+//  filingandquestion
+//
+//  Created by 難波映太 on 2025/11/18.
+//
+
+import SwiftUI
+
+@main
+struct filingandquestionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
